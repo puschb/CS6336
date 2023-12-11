18b062aa62230fe24af588c89e8a83ba228098a0
uhhJoyz <wbradford@renaissanceschool.org>
2023-12-11 15:46:15 -0500

    more dataset processing

--- data_analysis.py
+++ data_analysis.py
@@ -1,6 +1,76 @@
 import os
 import pandas as pd
 import numpy as np
+import json
+
+
+def sort_best(file):
+    """
+    sort the data from the current file by accuracy and separate
+    the parameters into their own columns
+    """
+    data = pd.read_csv(file)[
+        [
+            "params",
+            "mean_test_accuracy",
+            "mean_test_precision",
+            "mean_test_recall",
+            "mean_test_f1",
+            "mean_test_roc_auc",
+        ]
+    ]
+
+    # make a dict mapping from current colname to new colname
+    colnames = {
+        item: item.replace("mean_test_", "")
+        for item in list(data.columns.values.tolist())
+    }
+
+    data = data.rename(columns=colnames)
+    param_list = list(
+        json.loads(
+            data.iloc[0]["params"].replace("'", '"').replace("None", "null")
+        ).keys()
+    )
+
+    # convert to dict for iterative param list processing
+    data_dict = data.to_dict(orient="records")
+    for row in data_dict:
+        # replace single quotes with double quotes and None values with null
+        temp = json.loads(row["params"].replace("'", '"').replace("None", "null"))
+        row.update(temp)
+        row.pop("params")
+
+    # convert the dataframe back from dict
+    data = pd.DataFrame.from_dict(data_dict)
+    data = data.sort_values(
+        by=[
+            "accuracy",
+            "precision",
+            "recall",
+            "f1",
+            "roc_auc",
+        ],
+    )
+    # get the revised filename
+    fname = (
+        file.split("/")[-1].split("_")[0]
+        + "_"
+        + file.split(".")[-2].split("_")[-1]
+        + "_culled.csv"
+    )
+    # get the path to the revised file
+    path = "/".join(file.split("/")[:-1])
+    # folder must already exist
+    data.to_csv(path + "/culled/" + fname)
+    # print a message to the console if the correlation
+    # coefficient between accuracy and parameter is greater than 0.8
+    for p in param_list:
+        for m in ["accuracy", "precision", "recall", "f1", "roc_auc"]:
+            if data[p].dtypes == np.int64 or data[p].dtypes == np.float64:
+                if abs((data[m].corr(data[p]))) > 0.8:
+                    cor = data[m].corr(data[p])
+                    print(f"Correlation between {m} and {p} for {fname} is {cor}")
 
 
 def find_best(file):
@@ -34,7 +104,7 @@ def find_best(file):
     return best_classifier.iloc[0]
 
 
-def analyze_results(results_path="./results/"):
+def get_bests(results_path="./results/"):
     # check file extension
     files = [
         f
@@ -59,5 +129,21 @@ def analyze_results(results_path="./results/"):
     result.to_csv(results_path + "best/results.csv")
 
 
+def decipher_results(results_path="./results/"):
+    # check file extension
+    files = [
+        f
+        for f in os.listdir("./results/")
+        if ".csv" == f[-4:]
+        and any(
+            word in f
+            for word in ["decision", "k_neighbor", "logistic", "random_forest", "svm"]
+        )
+    ]
+
+    for f in files:
+        sort_best(results_path + f)
+
+
 if __name__ == "__main__":
-    analyze_results()
+    decipher_results()
--- data_loader.py
+++ data_loader.py
@@ -1,32 +1,33 @@
 import numpy as np
 
-#just loads the data, doesn't perform random sammpling for splits
+
+# just loads the data, doesn't perform random sammpling for splits
 def load_dataset1():
-  data_set_xs = []
-  data_set_ys = []
-  with open('./data/project3_dataset1.txt', "r", encoding='utf-8') as f:
-    for line in f:
-      vals = line.split('\t')
-      data_set_xs.append(vals[0:-1])
-      data_set_ys.append(vals[-1])
+    data_set_xs = []
+    data_set_ys = []
+    with open("./data/project3_dataset1.txt", "r", encoding="utf-8") as f:
+        for line in f:
+            vals = line.split("\t")
+            data_set_xs.append(vals[0:-1])
+            data_set_ys.append(vals[-1])
 
-  data_set_xs_numpy = np.array(data_set_xs,dtype='float64')
-  data_set_ys_numpy = np.array(data_set_ys, dtype='float64')
-  return data_set_xs_numpy, data_set_ys_numpy
+    data_set_xs_numpy = np.array(data_set_xs, dtype="float64")
+    data_set_ys_numpy = np.array(data_set_ys, dtype="float64")
+    return data_set_xs_numpy, data_set_ys_numpy
 
-#just loads the data, doesn't perform random sammpling for splits
-#converts present/absent to 1/0
+
+# just loads the data, doesn't perform random sammpling for splits
+# converts present/absent to 1/0
 def load_dataset2():
-  data_set_xs = []
-  data_set_ys = []
-  with open('./data/project3_dataset2.txt', "r", encoding='utf-8') as f:
-    for line in f:
-      vals = line.split('\t')
-      vals[4] = 1 if vals[4] == "Present" else 0
-      data_set_xs.append(vals[0:-1])
-      data_set_ys.append(vals[-1])
+    data_set_xs = []
+    data_set_ys = []
+    with open("./data/project3_dataset2.txt", "r", encoding="utf-8") as f:
+        for line in f:
+            vals = line.split("\t")
+            vals[4] = 1 if vals[4] == "Present" else 0
+            data_set_xs.append(vals[0:-1])
+            data_set_ys.append(vals[-1])
 
-  data_set_xs_numpy = np.array(data_set_xs,dtype='float64')
-  data_set_ys_numpy = np.array(data_set_ys, dtype='float64')
-  return data_set_xs_numpy, data_set_ys_numpy
-
+    data_set_xs_numpy = np.array(data_set_xs, dtype="float64")
+    data_set_ys_numpy = np.array(data_set_ys, dtype="float64")
+    return data_set_xs_numpy, data_set_ys_numpy
