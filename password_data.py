7dd83695fad90565ca8a174d72939b6c14ba3f19
uhhJoyz <wbradford@renaissanceschool.org>
2023-12-12 10:20:29 -0500

    added extra credit portion

--- password_data.py
+++ password_data.py
@@ -1,8 +1,18 @@
+import os
+
+os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+
 import hashlib
 import csv
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import normalize
+import tensorflow as tf
+from tensorflow.keras.models import Model
+from tensorflow.keras import layers, losses
+from tensorflow import keras
+from tqdm import tqdm
 
 
 def load_password_list():
@@ -56,14 +66,117 @@ def str_arr_to_bin_arr(dataset, standard_length=24):
     )
 
 
+def hex_arr_to_bin_arr(dataset, standard_length=16):
+    dataset = [np.binary_repr(int(s, 16), width=8 * standard_length) for s in dataset]
+    dataset = [[int(i) for i in list(item)] for item in dataset]
+    return dataset
+
+
 def parse_password_data():
     password_dataset = get_password_data()
 
     password = str_arr_to_bin_arr(password_dataset[:, 0])
-    hashes = str_arr_to_bin_arr(password_dataset[:, 1], standard_length=32)
+    hashes = hex_arr_to_bin_arr(password_dataset[:, 1], standard_length=16)
 
     return np.array(password, dtype="int8"), np.array(hashes, dtype="int8")
 
 
+def preprocess_data():
+    pass
+
+
+@keras.saving.register_keras_serializable()
+class Autoencoder(Model):
+    def __init__(self, shape):
+        super(Autoencoder, self).__init__()
+        self.latent_dim = 4
+        self.shape = shape
+        self.encoder = tf.keras.Sequential(
+            [layers.Flatten(), layers.Dense(self.latent_dim, activation="relu")]
+        )
+        self.decoder = tf.keras.Sequential(
+            [
+                layers.Dense(tf.math.reduce_prod(shape), activation="sigmoid"),
+                layers.Reshape(shape),
+            ]
+        )
+
+    def call(self, x):
+        encoded = self.encoder(x)
+        decoded = self.decoder(x)
+        return decoded
+
+    def get_encoded(self, x):
+        encoded = self.encoder(x)
+        return encoded
+
+    def get_decoded(self, x):
+        decoded = self.decoder(x)
+        return decoded
+
+
+def train_input_encoder():
+    characters = list(
+        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789~`!@#$%^&*()_-+={[]}|\:;"
+        "<,>.?/"
+    )
+    characters = str_arr_to_bin_arr(characters, standard_length=1)
+    shape = characters.shape[1:]
+    ae = Autoencoder(shape)
+    ae.compile(optimizer="adam", loss=losses.MeanSquaredError())
+
+    ae.fit(characters, characters, epochs=20000, shuffle=True)
+
+    ae.save("models/autoencoder.keras")
+
+
+def write_file(data, filename):
+    with open(f"data/password_data/{filename}", "w") as f:
+        writer = csv.writer(f, delimiter=",")
+        for row in data:
+            writer.writerow(row)
+
+
+def encode_dataset():
+    ae = tf.keras.models.load_model("models/autoencoder.keras")
+    pw, hash = parse_password_data()
+    epw = []
+    # this loop takes a very long time to execute
+    # because of all of the copies that occur inside of it
+    # unfortunately, there is no way to resolve this
+    for word in tqdm(pw):
+        encoded_word = []
+        for i in range(0, len(word), 8):
+            encoded_word.append(
+                ae.get_encoded(word[i : i + 8].reshape(1, 8)).numpy().tolist()
+            )
+        epw.append(np.asarray(encoded_word).flatten())
+
+    epw = np.asarray(epw)
+
+    write_file(pw, "binary_passwords.csv")
+    write_file(hash, "binary_hashes.csv")
+    write_file(epw, "encoded_passwords.csv")
+
+
+def load_password_data():
+    password = np.genfromtxt("./data/password_data/binary_passwords.csv", delimiter=",")
+    hashes = np.genfromtxt("./data/password_data/binary_hashes.csv", delimiter=",")
+    encoded_passwords = np.genfromtxt(
+        "./data/password_data/encoded_passwords.csv", delimiter=","
+    )
+
+    return password, hashes, encoded_passwords
+
+
+def tvt_split(data):
+    train = data[0 : int(len(data) * 0.8)]
+    validation = data[int(len(data) * 0.8) : int(len(data) * 0.9)]
+    test = data[int(len(data) * 0.9) :]
+    return train, validation, test
+
+
 if __name__ == "__main__":
-    write_hashed_data()
+    tf.get_logger().setLevel("FATAL")
+    tf.autograph.set_verbosity(1)
+    load_password_data()
--- password_tf.py
+++ password_tf.py
@@ -5,46 +5,118 @@ os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 
 import tensorflow as tf
 import tensorflow_datasets as tfds
-from tensorflow.keras import layers
-from password_data import parse_password_data
+from tensorflow.keras import layers, losses
+from tensorflow.keras.models import Model
+from tensorflow import keras
+from password_data import load_password_data, tvt_split
 from sklearn.preprocessing import normalize
+from sklearn.metrics import mean_squared_error
 import numpy as np
 
-tf.get_logger().setLevel("ERROR")
+tf.get_logger().setLevel("FATAL")
 tf.autograph.set_verbosity(1)
 
-if __name__ == "__main__":
+
+@keras.saving.register_keras_serializable()
+class Hasher(Model):
+    def __init__(self):
+        super(Hasher, self).__init__()
+        self.shape = (1, 96)
+        self.o_shape = (1, 128)
+        self.hidden = tf.keras.Sequential(
+            [
+                layers.Flatten(),
+                layers.Dense(256, activation="relu"),
+                layers.Dense(128, activation="relu"),
+                layers.Dense(tf.math.reduce_prod(self.o_shape), activation="sigmoid"),
+                layers.Reshape(self.o_shape),
+            ]
+        )
+
+    def call(self, x):
+        prediction = self.hidden(x)
+        return prediction
+
+
+@keras.saving.register_keras_serializable()
+class Reverser(Model):
+    def __init__(self):
+        super(Reverser, self).__init__()
+        self.shape = (1, 128)
+        self.o_shape = (1, 96)
+        self.hidden = tf.keras.Sequential(
+            [
+                layers.Flatten(),
+                layers.Dense(256, activation="relu"),
+                layers.Dense(tf.math.reduce_prod(self.o_shape), activation="sigmoid"),
+                layers.Reshape(self.o_shape),
+            ]
+        )
+
+    def call(self, x):
+        prediction = self.hidden(x)
+        return prediction
+
+
+def train_models():
     # builder = tfds.builder("rock_you")
     # x_train = builder.as_dataset(split="train[:50%]")
     # x_test = builder.as_dataset(split="train[50%:]")
-    pword, hash = parse_password_data()
+    pw, h, epw = load_password_data()
+    train_h, val_h, test_h = tvt_split(h)
+    train_epw, val_epw, test_epw = tvt_split(epw)
+    print(train_h)
 
-    plen, hlen = len(pword[0]), len(hash[0])
+    hasher = Hasher()
+    hasher.compile(optimizer="adam", loss=losses.MeanSquaredError())
+    hasher.fit(train_epw, train_h, epochs=2000, shuffle=True)
 
-    model = tf.keras.Sequential()
-    model.add(layers.Input(shape=(192, 1)))
-    model.add(layers.Dense(512, activation="relu"))
-    model.add(layers.Dense(1024, activation="relu"))
-    model.add(layers.Dense(512, activation="relu"))
-    model.add(layers.Dense(hlen))
+    hasher.save("models/hasher.keras")
 
-    loss = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+    reverser = Reverser()
+    reverser.compile(optimizer="adam", loss=losses.MeanSquaredError())
+    reverser.fit(train_h, train_epw, epochs=2000, shuffle=True)
 
-    # save model at each epoch
-    model_save = tf.keras.callbacks.ModelCheckpoint(
-        "models/checkpoint/",
-        save_weights_only=True,
-        monitor="val_loss",
-        save_freq="epoch",
-        mode="min",
-        save_best_only=True,
-    )
+    reverser.save("models/reverser.keras")
 
-    model.compile(optimizer="adam", loss=loss)
+    hasher.evaluate(val_epw, val_h)
+    reverser.evaluate(val_h, val_epw)
 
-    model.fit(pword, hash, batch_size=64, epochs=5, verbose=2, callbacks=[model_save])
-    model.evaluate(pword, hash, batch_size=64, verbose=2)
 
-    predictions = model.predict(pword)
-    print(predictions[0])
-    print(hash[0])
+def test_against_random_guessing():
+    # set up data
+    pw, h, epw = load_password_data()
+    train_h, val_h, test_h = tvt_split(h)
+    train_epw, val_epw, test_epw = tvt_split(epw)
+
+    # generate random outputs
+    random_hash = np.random.randint(2, size=val_h.shape)
+    random_epw = np.random.random_sample(val_epw.shape)
+
+    # load models
+    hasher = tf.keras.models.load_model("./models/hasher.keras")
+    reverser = tf.keras.models.load_model("./models/reverser.keras")
+
+    # evaluate models
+    hasher_loss = hasher.evaluate(val_epw, val_h)
+    reverser_loss = reverser.evaluate(val_h, val_epw)
+
+    # evaluate random guessing
+    r_hash_loss = mean_squared_error(random_hash, val_h)
+    r_epw_loss = mean_squared_error(random_epw, val_epw)
+
+    # compare against random outputs
+    hasher_score = r_hash_loss - hasher_loss
+    reverser_score = r_epw_loss - reverser_loss
+
+    print(f"hasher MSE: {hasher_loss}")
+    print(f"reverser MSE: {reverser_loss}")
+    print(f"random MSE - hasher MSE: {hasher_score}")
+    print(f"{r_hash_loss / hasher_loss} times better")
+    print(f"random MSE - reverser MSE: {reverser_score}")
+    print(f"{r_epw_loss / reverser_loss} times better")
+    return hasher_score, reverser_score
+
+
+if __name__ == "__main__":
+    test_against_random_guessing()
